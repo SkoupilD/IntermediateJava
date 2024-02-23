92ccd6c96c6a230dbcb80b164b93105dcdba2b8b
SkoupilD <skoupil.d@icloud.com>
2024-02-23 10:31:20 +0100

    project done

--- GymThreads/src/Gym.java
+++ GymThreads/src/Gym.java
@@ -1,2 +1,58 @@
+import java.util.Map;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 public class Gym {
+    private final int totalGymMembers;
+    private Map<MachineType, Integer> availableMachines;
+
+    public Gym(int totalGymMembers, Map<MachineType, Integer> availableMachines) {
+        this.totalGymMembers = totalGymMembers;
+        this.availableMachines = availableMachines;
+    }
+
+    public void openForTheDay() {
+        List<Thread> gymMembersRoutines;
+        gymMembersRoutines = IntStream.rangeClosed(1, this.totalGymMembers)
+                .mapToObj((id) -> {
+                    Member member = new Member(id);
+                    return new Thread(() -> {
+                        try {
+                            member.performRoutine();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                })
+                .collect(Collectors.toList());
+        Thread supervisor = createSupervisor(gymMembersRoutines);
+        supervisor.start();
+
+        gymMembersRoutines.forEach((t) -> t.start());
+    }
+
+    private Thread createSupervisor(List<Thread> threads) {
+        Thread supervisor = new Thread(() -> {
+            while (true) {
+                List<String> runningThreads = threads.stream()
+                        .filter(Thread::isAlive)
+                        .map(Thread::getName)
+                        .collect(Collectors.toList());
+                if (runningThreads.isEmpty()) {
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + " - " + runningThreads.size() +
+                        " members currently exercising: " + runningThreads + " \n");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + " - All members are finished exercising!");
+        });
+        supervisor.setName("Gym Staff");
+        return supervisor;
+    }
 }
--- GymThreads/src/Main.java
+++ GymThreads/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gym globoGym = new Gym(5, new HashMap<>() {
+            {
+                put(MachineType.LEGPRESSMACHINE, 5);
+                put(MachineType.BARBELL, 5);
+                put(MachineType.SQUATMACHINE, 5);
+                put(MachineType.LEGEXTENSIONMACHINE, 5);
+                put(MachineType.LEGCURLMACHINE, 5);
+                put(MachineType.LATPULLDOWNMACHINE, 5);
+                put(MachineType.CABLECROSSOVERMACHINE, 5);
+            }
+        });
+
+        globoGym.openForTheDay();
     }
 }
